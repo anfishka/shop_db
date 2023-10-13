022c86c8009c3119f78750d65bde614e0c442a3c
Hanna Borodina <venera131916@gmail.com>
2023-10-14 00:36:24 +0300

    init some config files

---
+++ src/db/scema.ts
@@ -0,0 +1,15 @@
+import {pgTable, serial, varchar, text, integer, doublePrecision} from 'drizzle-orm/pg-core';
+
+export const products = pgTable('products', {
+    id:serial('id').primaryKey(),
+    product_name: varchar('product_name', {length:100}).notNull(),
+    product_category: varchar('product_category', {length:100}).notNull(),
+    product_description: text('product_description').notNull(),
+    product_price: doublePrecision('product_price').notNull(),
+    product_stock: integer('product_stock').notNull(),
+    product_image: text('product_image').notNull(),
+});
+
+// export const orders = {}
+
+// export const order_items = {}
